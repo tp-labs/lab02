57abfee30aad69804503d69d0da81a5549a73fdc
YodJI27 <43516919+YodJI27@users.noreply.github.com>
2019-05-17 19:26:17 +0300

    commit

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,2 +1,7 @@
-#include <hello_world.hpp> using namespace std
-int main() { cout << "hello world" << endl; } 
+#include <hello_world.hpp> 
+using namespace std
+// commit
+int main() 
+{ 
+    cout << "hello world" << endl; 
+} 
