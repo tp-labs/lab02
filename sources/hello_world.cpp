524ed78852c60e1646884bdd5bd37b37cb6da8cc
keyow <kirill500123@gmail.com>
2022-03-09 00:01:19 +0300

    added comments

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -2,11 +2,16 @@
 #include <string>
 
 
+/*
+	Программа выводит hello world от пользователя
+	(Ниже представлен хороший кодстайл)
+*/
+
 int main() {
-	std::string username;
+	std::string username; // Имя пользователя
 	
-	std::getline(std::cin, username);
- 	std::cout << "Hello world from " << username << std::endl;
+	std::getline(std::cin, username); // Ввод имени пользователя
+ 	std::cout << "Hello world from " << username << std::endl; // Вывод
 
 	return 0;
 }
